1ad7bbbfe24b61d4bef630166f7694397062dfbb
Greeshma <greeshmap508@gmail.com>
2022-11-22 15:36:15 +0530

    Add project files.

---
+++ seventh/student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seventh
+{
+    internal class student
+    {
+        public int rollNumber;
+        public string name;
+
+        public student()
+        {
+            rollNumber = 208765;
+            name = "maya";
+        }
+        public void Displaydetails()
+        {
+            Console.WriteLine($"Rollnumber is {rollNumber} \n Name is {name}");
+        }
+    }
+      public class test
+        {
+            static void Main(string[] args)
+            {
+                student r = new student();
+                r.Displaydetails();
+            }
+        }
+    
+}
